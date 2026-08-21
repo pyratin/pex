311f521306ae9acee804b5709deeef59ba811d99
pyratin <pyratin@hotmail.com>
2026-08-21 18:40:07 +0530

    Container: Transform / Origin

--- source/Route_/Home.js
+++ source/Route_/Home.js
@@ -1,194 +1,286 @@
-import { useRef, useState, useEffect } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
+import { createRoot } from 'react-dom/client';
 import { useExtend } from '@pixi/react';
 import { useShallow } from 'zustand/react/shallow';
-import _ from 'lodash';
-import '@pixi/layout';
+import * as pixiLayout from '@pixi/layout';
 import { LayoutContainer } from '@pixi/layout/components';
 import * as pixiJs from 'pixi.js';
-import { Assets, Texture, Sprite, Rectangle } from 'pixi.js';
+import { Graphics } from 'pixi.js';
 import { useGSAP } from '@gsap/react';
 import gsap from 'gsap';
 import { PixiPlugin } from 'gsap/PixiPlugin';
 
 import useStore from '#/component/useStore';
+import style from './index.module.scss';
 
 gsap.registerPlugin(useGSAP, PixiPlugin);
 PixiPlugin.registerPIXI(pixiJs);
 
-const rectangleGet = _.memoize((textureDimension) => {
-  const padding = (() => {
-    const { width, height } = textureDimension;
+const gridDimension = 50;
 
-    return Math.max(width, height);
-  })();
+/** @type {(index: number) => number} */
+const gridOffsetGet = (index) => index * gridDimension;
 
-  const { width, height } = (() => {
-    const { getState } = useStore;
-
-    const {
-      displayDefinition: { dimension }
-    } = getState();
-
-    return dimension;
-  })();
-
-  return new Rectangle(
-    -padding,
-    -padding,
-    width + padding * 2,
-    height + padding * 2
-  );
+const gridStrokeDefinition = /** @type {pixiJs.StrokeStyle} */ ({
+  width: 1,
+  color: 0xcccccc
 });
 
-const Sprite_ = () => {
-  useExtend({ LayoutContainer, Sprite });
+const dimension = { width: 150, height: 100 };
 
-  const { displayDefinitionDimension } = useStore(
-    useShallow(({ displayDefinition: { dimension } }) => ({
-      displayDefinitionDimension: dimension
-    }))
+const transformOriginInitializedGet = () =>
+  Object.values(dimension).map((value) => value / 2);
+
+const Grid = () => {
+  useExtend({ Graphics });
+
+  const { width, height } = useStore(
+    useShallow(
+      ({
+        displayDefinition: {
+          dimension: { width, height }
+        }
+      }) => ({
+        width,
+        height
+      })
+    )
   );
+
+  return (
+    <>
+      {Array.from({ length: Math.ceil(width / gridDimension) }).map(
+        (_, index) => (
+          <pixiGraphics
+            key={index}
+            draw={(graphics) =>
+              graphics
+                .moveTo(gridOffsetGet(index), 0)
+                .lineTo(gridOffsetGet(index), height)
+                .stroke(gridStrokeDefinition)
+            }
+          />
+        )
+      )}
+
+      {Array.from({ length: Math.ceil(height / gridDimension) }).map(
+        (_, index) => (
+          <pixiGraphics
+            key={index}
+            draw={(graphics) =>
+              graphics
+                .moveTo(0, gridOffsetGet(index))
+                .lineTo(width, gridOffsetGet(index))
+                .stroke(gridStrokeDefinition)
+            }
+          />
+        )
+      )}
+    </>
+  );
+};
+
+const OriginMarker = () => {
+  useExtend({ Graphics });
+
+  return (
+    <pixiGraphics
+      label='OriginMarker'
+      draw={(graphics) =>
+        graphics
+          .moveTo(-10, 0)
+          .lineTo(10, 0)
+          .moveTo(0, -10)
+          .lineTo(0, 10)
+          .stroke({ width: 4, color: 0xff0000 })
+      }
+    />
+  );
+};
+
+const LayoutContainer_ = () => {
+  useExtend({ LayoutContainer, Graphics });
 
   const ref = useRef(undefined);
 
-  const [texture, textureSet] = useState(Texture.EMPTY);
-
-  useEffect(() => {
-    Assets.load('/asset/image/eggHead.png').then(textureSet);
-  }, []);
-
   useGSAP(
     () => {
-      const refCurrent = /** @type {pixiJs.Container} */ (ref.current);
-
-      const _angle = Math.random() * 0.5;
-
-      const speed = Math.random() * 2 + 2;
-
-      const fn = () => {
-        const angle = (() => {
-          const { angle } = refCurrent;
-
-          return angle + _angle;
-        })();
-
-        const _position = (() => {
-          const {
-            position: { x, y }
-          } = refCurrent;
-
-          return /** @type {{ x: number; y: number }} */ (
-            Object.entries({ x, y }).reduce(
-              (memo, [key, value], index) => ({
-                ...memo,
-                [key]:
-                  value +
-                  Math[!index ? 'sin' : 'cos'](
-                    (-angle - 90) * pixiJs.DEG_TO_RAD
-                  ) *
-                    speed
-              }),
-              {}
-            )
-          );
-        })();
-
-        const position = {
-          ..._position,
-          ...(() => {
-            const rectangle = rectangleGet(
-              (() => {
-                const { width, height } = texture;
-
-                return { width, height };
-              })()
-            );
-
-            switch (true) {
-              case _position.x < rectangle.x:
-                return { x: _position.x + rectangle.width };
-
-              case _position.x > rectangle.x + rectangle.width:
-                return { x: _position.x - rectangle.width };
-
-              case _position.y < rectangle.y:
-                return { y: _position.y + rectangle.height };
-
-              case _position.y > rectangle.y + rectangle.height:
-                return { y: _position.y - rectangle.height };
-            }
-          })()
-        };
-
-        Object.assign(
-          refCurrent,
-          /** @type {pixiJs.ContainerOptions} */ ({
-            position,
-            angle
-          })
-        );
-      };
-
-      texture !== Texture.EMPTY && gsap.ticker.add(fn);
-
-      return () => gsap.ticker.remove(fn);
+      gsap.to(ref.current, {
+        pixi: { angle: 360 },
+        repeat: -1,
+        ease: 'none',
+        duration: 5
+      });
     },
-    { dependencies: [texture] }
+    { dependencies: [] }
   );
 
   return (
     <pixiLayoutContainer
       ref={ref}
+      label='LayoutContainer_'
       layout={{
-        position: 'absolute',
-        borderWidth: 0,
-        borderColor: 0x00ff00
+        ...dimension
       }}
-      position={(() => {
-        const { width, height } = displayDefinitionDimension;
-
-        const { width: _width, height: _height } = texture;
-
-        return /** @type {{ x: number; y: number }} */ (
-          Object.entries({
-            x: width - _width,
-            y: height - _height
-          }).reduce(
-            (memo, [key, value]) => ({
-              ...memo,
-              [key]: Math.random() * value
-            }),
-            {}
-          )
-        );
-      })()}
-      angle={(() => Math.random() * 360)()}
+      position={{ x: 100, y: 100 }}
     >
-      <pixiSprite
-        texture={texture}
-        layout={{}}
-        angle={-90}
-        tint={(() => Math.random() * 0xffffff)()}
+      <pixiGraphics
+        draw={(graphics) =>
+          graphics
+            .rect(
+              ...(() => {
+                const { width, height } = dimension;
+
+                return /** @type {const} */ ([0, 0, width, height]);
+              })()
+            )
+            .fill({ color: 0x3489db })
+        }
       />
+
+      <OriginMarker />
     </pixiLayoutContainer>
+  );
+};
+
+const Control = ({ transformOrigin, maxCollection, transformOriginSet }) => {
+  const definitionCollection = ['x', 'y'].map((key, index) => ({
+    key,
+    value: transformOrigin[index],
+    max: maxCollection[index]
+  }));
+
+  return (
+    <div className={style.Control}>
+      <h3>Adjust Origin</h3>
+
+      <div>
+        {definitionCollection.map(({ key, value, max }, index) => (
+          <div key={index}>
+            <label htmlFor={key} className='form-label'>
+              {key}:
+            </label>
+
+            <input
+              type='range'
+              className='form-range'
+              id={key}
+              value={value}
+              min={0}
+              max={max}
+              step={5}
+              onChange={({ target: { valueAsNumber } }) => {
+                transformOriginSet(
+                  /** @type {(transformOrigin: number[]) => number[]} */
+                  (transformOrigin) => {
+                    return transformOrigin.map((value, _index) => {
+                      return _index === index ? valueAsNumber : value;
+                    });
+                  }
+                );
+              }}
+            />
+          </div>
+        ))}
+      </div>
+
+      <button
+        className='btn btn-outline-light btn-sm'
+        onClick={() => transformOriginSet(transformOriginInitializedGet())}
+      >
+        Reset
+      </button>
+    </div>
   );
 };
 
 const Home = () => {
   useExtend({ LayoutContainer });
 
+  const ref = useRef(undefined);
+
+  const rootRef = useRef(undefined);
+
+  const [transformOrigin, transformOriginSet] = useState(
+    transformOriginInitializedGet()
+  );
+
+  useEffect(() => {
+    const refCurrent = /** @type {pixiJs.Container} */ (ref.current);
+
+    Object.assign(
+      refCurrent.getChildByLabel('LayoutContainer_'),
+      /** @type {pixiJs.ContainerOptions} */ ({
+        layout: /** @type {pixiLayout.LayoutOptions} */ ({
+          transformOrigin: transformOrigin.join(', ')
+        })
+      })
+    );
+
+    Object.assign(
+      refCurrent
+        .getChildByLabel('LayoutContainer_')
+        .getChildByLabel('OriginMarker'),
+      /** @type {pixiJs.ContainerOptions} */ ({
+        position: transformOrigin.reduce(
+          (memo, value, index) => ({
+            ...memo,
+            [!index ? 'x' : 'y']: value
+          }),
+          {}
+        )
+      })
+    );
+  }, [transformOrigin]);
+
+  useEffect(() => {
+    const element = document.createElement('div');
+
+    document.body.appendChild(element);
+
+    Object.assign(
+      element.style,
+      /** @type {React.CSSProperties} */ ({
+        position: 'absolute',
+        top: 0,
+        right: 0
+      })
+    );
+
+    const root = createRoot(element);
+
+    Object.assign(rootRef, { current: root });
+
+    return () => {
+      root.unmount();
+
+      element.remove();
+    };
+  }, []);
+
+  useEffect(() => {
+    rootRef.current.render(
+      <Control
+        transformOrigin={transformOrigin}
+        maxCollection={Object.values(dimension)}
+        transformOriginSet={transformOriginSet}
+      />
+    );
+  }, [transformOrigin, transformOriginSet]);
+
   return (
     <pixiLayoutContainer
+      ref={ref}
       layout={{
-        position: 'relative',
         flex: 1,
         borderWidth: 0,
-        borderColor: 0xff0000
+        borderColor: 0xff0000,
+        backgroundColor: 0xffffff
       }}
     >
-      {Array.from({ length: 20 }).map((_, index) => (
-        <Sprite_ key={index} />
-      ))}
+      <Grid />
+
+      <LayoutContainer_ />
     </pixiLayoutContainer>
   );
 };
--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -1,5 +1,14 @@
 /** @type {import('stylelint').Config} */
 export default {
   extends: ['stylelint-config-standard-scss'],
-  ignoreFiles: ['!**/*.scss']
+  ignoreFiles: ['!**/*.scss'],
+  rules: {
+    'selector-pseudo-class-no-unknown': [
+      true,
+      {
+        ignorePseudoClasses: ['global', 'local']
+      }
+    ],
+    'selector-class-pattern': null
+  }
 };
