d6ca5f2c768b3ad1f3f738c15492f5922cc41265
pyratin <pyratin@hotmail.com>
2026-08-21 17:57:01 +0530

    Container: Tinting

--- source/Route_/Home.js
+++ source/Route_/Home.js
@@ -1,22 +1,47 @@
 import { useRef, useState, useEffect } from 'react';
-import { useExtend, useApplication } from '@pixi/react';
+import { useExtend } from '@pixi/react';
 import { useShallow } from 'zustand/react/shallow';
+import _ from 'lodash';
 import '@pixi/layout';
 import { LayoutContainer } from '@pixi/layout/components';
 import * as pixiJs from 'pixi.js';
-import {
-  Assets,
-  Texture,
-  Graphics,
-  Sprite,
-  BlurFilter,
-  Rectangle
-} from 'pixi.js';
+import { Assets, Texture, Sprite, Rectangle } from 'pixi.js';
+import { useGSAP } from '@gsap/react';
+import gsap from 'gsap';
+import { PixiPlugin } from 'gsap/PixiPlugin';
 
 import useStore from '#/component/useStore';
 
+gsap.registerPlugin(useGSAP, PixiPlugin);
+PixiPlugin.registerPIXI(pixiJs);
+
+const rectangleGet = _.memoize((textureDimension) => {
+  const padding = (() => {
+    const { width, height } = textureDimension;
+
+    return Math.max(width, height);
+  })();
+
+  const { width, height } = (() => {
+    const { getState } = useStore;
+
+    const {
+      displayDefinition: { dimension }
+    } = getState();
+
+    return dimension;
+  })();
+
+  return new Rectangle(
+    -padding,
+    -padding,
+    width + padding * 2,
+    height + padding * 2
+  );
+});
+
 const Sprite_ = () => {
-  useExtend({ Sprite });
+  useExtend({ LayoutContainer, Sprite });
 
   const { displayDefinitionDimension } = useStore(
     useShallow(({ displayDefinition: { dimension } }) => ({
@@ -24,71 +49,127 @@ const Sprite_ = () => {
     }))
   );
 
-  const [texture, textureSet] = useState(Texture.EMPTY);
-
-  useEffect(() => {
-    Assets.load('/asset/image/bg_grass.jpg').then(textureSet);
-  }, []);
-
-  return <pixiSprite texture={texture} {...displayDefinitionDimension} />;
-};
-
-const LayoutContainer_ = () => {
-  const radius = 90;
-
-  const blurStrength = 10;
-
-  const dimension = (radius + blurStrength) * 2;
-
-  useExtend({ LayoutContainer, Graphics, Sprite });
-
-  const {
-    app: { renderer }
-  } = useApplication();
-
   const ref = useRef(undefined);
 
   const [texture, textureSet] = useState(Texture.EMPTY);
 
   useEffect(() => {
-    const refCurrentGraphics = /** @type {pixiJs.Graphics} */ (
-      /** @type {pixiJs.Container} */ (ref.current).getChildAt(0)
-    );
+    Assets.load('/asset/image/eggHead.png').then(textureSet);
+  }, []);
 
-    textureSet(
-      renderer.generateTexture({
-        target: refCurrentGraphics,
-        frame: new Rectangle(0, 0, dimension, dimension)
-      })
-    );
+  useGSAP(
+    () => {
+      const refCurrent = /** @type {pixiJs.Container} */ (ref.current);
 
-    Object.assign(
-      refCurrentGraphics,
-      /** @type {pixiJs.GraphicsOptions} */ ({
-        visible: false
-      })
-    );
-  }, [dimension, renderer]);
+      const _angle = Math.random() * 0.5;
+
+      const speed = Math.random() * 2 + 2;
+
+      const fn = () => {
+        const angle = (() => {
+          const { angle } = refCurrent;
+
+          return angle + _angle;
+        })();
+
+        const _position = (() => {
+          const {
+            position: { x, y }
+          } = refCurrent;
+
+          return /** @type {{ x: number; y: number }} */ (
+            Object.entries({ x, y }).reduce(
+              (memo, [key, value], index) => ({
+                ...memo,
+                [key]:
+                  value +
+                  Math[!index ? 'sin' : 'cos'](
+                    (-angle - 90) * pixiJs.DEG_TO_RAD
+                  ) *
+                    speed
+              }),
+              {}
+            )
+          );
+        })();
+
+        const position = {
+          ..._position,
+          ...(() => {
+            const rectangle = rectangleGet(
+              (() => {
+                const { width, height } = texture;
+
+                return { width, height };
+              })()
+            );
+
+            switch (true) {
+              case _position.x < rectangle.x:
+                return { x: _position.x + rectangle.width };
+
+              case _position.x > rectangle.x + rectangle.width:
+                return { x: _position.x - rectangle.width };
+
+              case _position.y < rectangle.y:
+                return { y: _position.y + rectangle.height };
+
+              case _position.y > rectangle.y + rectangle.height:
+                return { y: _position.y - rectangle.height };
+            }
+          })()
+        };
+
+        Object.assign(
+          refCurrent,
+          /** @type {pixiJs.ContainerOptions} */ ({
+            position,
+            angle
+          })
+        );
+      };
+
+      texture !== Texture.EMPTY && gsap.ticker.add(fn);
+
+      return () => gsap.ticker.remove(fn);
+    },
+    { dependencies: [texture] }
+  );
 
   return (
     <pixiLayoutContainer
       ref={ref}
       layout={{
         position: 'absolute',
-        width: dimension,
-        height: dimension,
         borderWidth: 0,
         borderColor: 0x00ff00
       }}
-    >
-      <pixiGraphics
-        draw={(graphics) =>
-          graphics.circle(dimension / 2, dimension / 2, radius).fill(0xffffff)
-        }
-        filters={[new BlurFilter({ strength: blurStrength })]}
-      />
+      position={(() => {
+        const { width, height } = displayDefinitionDimension;
 
-      <pixiSprite texture={texture} />
+        const { width: _width, height: _height } = texture;
+
+        return /** @type {{ x: number; y: number }} */ (
+          Object.entries({
+            x: width - _width,
+            y: height - _height
+          }).reduce(
+            (memo, [key, value]) => ({
+              ...memo,
+              [key]: Math.random() * value
+            }),
+            {}
+          )
+        );
+      })()}
+      angle={(() => Math.random() * 360)()}
+    >
+      <pixiSprite
+        texture={texture}
+        layout={{}}
+        angle={-90}
+        tint={(() => Math.random() * 0xffffff)()}
+      />
     </pixiLayoutContainer>
   );
 };
@@ -96,50 +177,18 @@ const LayoutContainer_ = () => {
 const Home = () => {
   useExtend({ LayoutContainer });
 
-  const ref = useRef(undefined);
-
-  useEffect(() => {
-    const refCurrent = /** @type {pixiJs.Container} */ (ref.current);
-
-    refCurrent.getChildAt(0).setMask({
-      mask: refCurrent.getChildAt(1).getChildAt(1)
-    });
-  }, []);
-
   return (
     <pixiLayoutContainer
-      ref={ref}
       layout={{
         position: 'relative',
         flex: 1,
         borderWidth: 0,
         borderColor: 0xff0000
       }}
-      eventMode='static'
-      cursor='pointer'
-      onPointerMove={
-        /** @type {(event: pixiJs.FederatedPointerEvent) => void} */
-        ({ client, currentTarget }) => {
-          const currentTargetLayoutContainer = currentTarget.getChildAt(1);
-
-          Object.assign(
-            currentTargetLayoutContainer,
-            /** @type {pixiJs.ContainerOptions} */ ({
-              position: (() => {
-                const { x, y } = client;
-
-                const { width, height } = currentTargetLayoutContainer;
-
-                return { x: x - width / 2, y: y - height / 2 };
-              })()
-            })
-          );
-        }
-      }
     >
-      <Sprite_ />
-
-      <LayoutContainer_ />
+      {Array.from({ length: 20 }).map((_, index) => (
+        <Sprite_ key={index} />
+      ))}
     </pixiLayoutContainer>
   );
 };
