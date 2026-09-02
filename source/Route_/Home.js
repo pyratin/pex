3cedfae22d7e07185800367500307fc1c22a34a5
pyratin <pyratin@hotmail.com>
2026-09-02 20:21:07 +0530

    Container: Cache As Texture / Optimization

--- source/Route_/Home.js
+++ source/Route_/Home.js
@@ -3,96 +3,105 @@ import { useExtend } from '@pixi/react';
 import { useShallow } from 'zustand/react/shallow';
 import { LayoutContainer } from '@pixi/layout/components';
 import * as pixiJs from 'pixi.js';
-import { Assets, Texture, Sprite, Text } from 'pixi.js';
+import { Assets, Texture, Sprite } from 'pixi.js';
 import { useGSAP } from '@gsap/react';
 import gsap from 'gsap';
 import { PixiPlugin } from 'gsap/PixiPlugin';
-import 'pixi.js/advanced-blend-modes';
 
 import useStore from '#/component/useStore';
 
 gsap.registerPlugin(useGSAP, PixiPlugin);
 PixiPlugin.registerPIXI(pixiJs);
 
-const LayoutContainer__ = ({ index, dimension }) => {
-  const blendMode = /** @type {pixiJs.BLEND_MODES} */ (
-    [
-      'normal',
-      'add',
-      'screen',
-      'darken',
-      'lighten',
-      'color-dodge',
-      'color-burn',
-      'linear-burn',
-      'linear-dodge',
-      'linear-light',
-      'hard-light',
-      'soft-light',
-      'pin-light',
-      'difference',
-      'exclusion',
-      'overlay',
-      'saturation',
-      'color',
-      'luminosity',
-      'add-npm',
-      'subtract',
-      'divide',
-      'vivid-light',
-      'hard-mix',
-      'negation'
-    ][index]
-  );
+const LayoutContainer_ = ({ index }) => {
+  useExtend({ LayoutContainer, Sprite });
 
-  useExtend({ LayoutContainer, Sprite, Text });
-
-  const { displayScaleFactor } = useStore(
-    useShallow(({ displayDefinition: { scaleFactor } }) => ({
-      displayScaleFactor: scaleFactor
+  const { displayDimension } = useStore(
+    useShallow(({ displayDefinition: { dimension } }) => ({
+      displayDimension: dimension
     }))
   );
 
   const ref = useRef(undefined);
 
-  const [textureCollection, textureCollectionSet] = useState(
-    Array.from({ length: 2 }).map(() => Texture.EMPTY)
-  );
+  const [texture, textureSet] = useState(Texture.EMPTY);
 
   useEffect(() => {
-    const assetAliasCollection = ['panda', 'rainbow-gradient'];
+    Assets.load('/asset/sprite/monsters.json').then(({ textures }) => {
+      const textureCollection = Object.values(textures);
 
-    Assets.load(
-      assetAliasCollection.map((alias) => ({
-        alias,
-        src: `/asset/image/${alias}.png`
-      }))
-    ).then((assetObject) =>
-      textureCollectionSet(
-        assetAliasCollection.map((alias) => assetObject[alias])
-      )
-    );
-
-    Assets.load({
-      alias: 'ShortStack',
-      src: '/asset/font/ShortStack-Regular.ttf',
-      data: { family: 'ShortStack' }
+      textureSet(textureCollection[index % textureCollection.length]);
     });
-  }, []);
+  }, [index]);
 
   useGSAP(
     () => {
-      gsap.to(
-        /** @type {pixiJs.Sprite} */ (
-          /** @type {pixiJs.Container} */ (ref.current).getChildAt(0)
-        ),
-        {
-          pixi: { angle: 360 },
-          repeat: -1,
-          ease: 'none',
-          duration: 5
-        }
-      );
+      gsap.to(ref.current, {
+        pixi: { angle: 360 },
+        repeat: -1,
+        ease: 'none',
+        duration: 5
+      });
+    },
+    { dependencies: [] }
+  );
+
+  return (
+    <pixiLayoutContainer
+      ref={ref}
+      layout={{
+        position: 'absolute',
+        borderWidth: 0,
+        borderColor: 0x00ff00
+      }}
+      position={(() => {
+        const { width, height } = displayDimension;
+
+        const { width: _width, height: _height } = texture;
+
+        return /** @type {{ x: number; y: number }} */ (
+          Object.entries({
+            x: width - _width,
+            y: height - _height
+          }).reduce(
+            (memo, [key, value]) => ({
+              ...memo,
+              [key]: Math.random() * value
+            }),
+            {}
+          )
+        );
+      })()}
+    >
+      <pixiSprite
+        texture={texture}
+        layout={{}}
+        tint={(() => Math.random() * 0xffffff)()}
+      />
+    </pixiLayoutContainer>
+  );
+};
+
+const Home = () => {
+  useExtend({ LayoutContainer });
+
+  const ref = useRef(undefined);
+
+  useGSAP(
+    () => {
+      /** @type {(_: number, __: number, frame: number) => void} */
+      const fn = (_, __, frame) =>
+        Object.assign(
+          ref.current,
+          /** @type {pixiJs.ContainerOptions} */ ({
+            scale: Math.sin(frame * 0.01),
+            angle: frame
+          })
+        );
+
+      gsap.ticker.add(fn);
+
+      return () => gsap.ticker.remove(fn);
     },
     { dependencies: [] }
   );
@@ -102,110 +111,26 @@ const LayoutContainer__ = ({ index, dimension }) => {
       ref={ref}
       layout={{
         position: 'relative',
-        width: dimension,
-        height: dimension,
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'space-between',
-        borderWidth: 1,
-        borderColor: 0x000000,
-        backgroundColor: 0xffffff
-      }}
-    >
-      <pixiSprite
-        texture={textureCollection[0]}
-        layout={{
-          width: dimension * 0.75,
-          objectFit: 'contain'
-        }}
-      />
-
-      <pixiSprite
-        texture={textureCollection[1]}
-        layout={{
-          position: 'absolute',
-          width: '100%',
-          height: '100%'
-        }}
-        blendMode={blendMode}
-      />
-
-      <pixiLayoutContainer
-        layout={{
-          justifyContent: 'center',
-          alignItems: 'center',
-          padding: 10,
-          marginBottom: 5,
-          borderWidth: 1,
-          borderColor: 0x000000,
-          borderRadius: 4
-        }}
-      >
-        <pixiText
-          text={blendMode}
-          layout={{}}
-          style={{
-            fontFamily: 'ShortStack',
-            fontSize: 12 * displayScaleFactor,
-            fontWeight: 'bolder'
-          }}
-        />
-      </pixiLayoutContainer>
-    </pixiLayoutContainer>
-  );
-};
-
-const LayoutContainer_ = () => {
-  useExtend({ LayoutContainer });
-
-  const { dimension } = useStore(
-    useShallow(
-      ({
-        displayDefinition: {
-          widMaximum,
-          dimension: { width }
-        }
-      }) => ({
-        dimension: Math.min(widMaximum, width)
-      })
-    )
-  );
-
-  return (
-    <pixiLayoutContainer
-      layout={{
-        width: dimension,
-        height: dimension,
-        flexWrap: 'wrap',
-        borderWidth: 0,
-        borderColor: 0x00ff00
-      }}
-    >
-      {Array.from({ length: 25 }).map((_, index, collection) => (
-        <LayoutContainer__
-          key={index}
-          index={index}
-          dimension={Math.floor(dimension / collection.length ** 0.5)}
-        />
-      ))}
-    </pixiLayoutContainer>
-  );
-};
-
-const Home = () => {
-  useExtend({ LayoutContainer });
-
-  return (
-    <pixiLayoutContainer
-      layout={{
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
         borderWidth: 0,
         borderColor: 0xff0000
       }}
+      eventMode='static'
+      cursor='pointer'
+      onPointerTap={
+        /** @type {(event: pixiJs.FederatedPointerEvent) => void} */
+        (event) => {
+          event.stopPropagation();
+
+          const { currentTarget } = event;
+
+          currentTarget.cacheAsTexture(!currentTarget.isCachedAsTexture);
+        }
+      }
     >
-      <LayoutContainer_ />
+      {Array.from({ length: 100 }).map((_, index) => (
+        <LayoutContainer_ key={index} index={index} />
+      ))}
     </pixiLayoutContainer>
   );
 };
