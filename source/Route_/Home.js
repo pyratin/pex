886da76635d74439b12811f3fd707e3fe2782cb3
pyratin <pyratin@hotmail.com>
2026-09-03 08:57:01 +0530

    Container: Mask / Filters / Blur

--- source/Route_/Home.js
+++ source/Route_/Home.js
@@ -1,13 +1,101 @@
-import { useRef, useEffect } from 'react';
-import { useExtend } from '@pixi/react';
+import { useRef, useState, useEffect } from 'react';
+import { useExtend, useApplication } from '@pixi/react';
+import { useShallow } from 'zustand/react/shallow';
 import { LayoutContainer } from '@pixi/layout/components';
 import * as pixiJs from 'pixi.js';
-import { Graphics } from 'pixi.js';
+import {
+  Assets,
+  Texture,
+  Graphics,
+  BlurFilter,
+  Sprite,
+  Rectangle
+} from 'pixi.js';
 
-const dimension = 200;
+import useStore from '#/component/useStore';
+
+const radius = 90;
+
+const blurStrength = 10;
+
+const dimension = (radius + blurStrength) * 2;
+
+const Sprite_ = () => {
+  useExtend({ Sprite });
+
+  const { displayDimension } = useStore(
+    useShallow(({ displayDefinition: { dimension } }) => ({
+      displayDimension: dimension
+    }))
+  );
+
+  const [texture, textureSet] = useState(Texture.EMPTY);
+
+  useEffect(() => {
+    Assets.load('/asset/image/bg_grass.jpg').then(textureSet);
+  }, []);
+
+  return <pixiSprite texture={texture} {...displayDimension} />;
+};
 
 const LayoutContainer_ = () => {
-  useExtend({ LayoutContainer, Graphics });
+  useExtend({ LayoutContainer, Graphics, Sprite });
+
+  const {
+    app: { renderer }
+  } = useApplication();
+
+  const ref = useRef(undefined);
+
+  const [texture, textureSet] = useState(Texture.EMPTY);
+
+  useEffect(() => {
+    const refCurrentGraphics = /** @type {pixiJs.Graphics} */ (
+      /** @type {pixiJs.Container} */ (ref.current).getChildAt(0)
+    );
+
+    textureSet(
+      renderer.generateTexture({
+        target: refCurrentGraphics,
+        frame: new Rectangle(0, 0, dimension, dimension)
+      })
+    );
+
+    Object.assign(
+      refCurrentGraphics,
+      /** @type {pixiJs.GraphicsOptions} */ ({
+        visible: false
+      })
+    );
+  }, [renderer]);
+
+  return (
+    <pixiLayoutContainer
+      ref={ref}
+      layout={{
+        position: 'absolute',
+        width: dimension,
+        height: dimension,
+        borderWidth: 0,
+        borderColor: 0x00ff00
+      }}
+    >
+      <pixiGraphics
+        draw={(graphics) =>
+          graphics
+            .circle(dimension / 2, dimension / 2, radius)
+            .fill({ color: 0xffffff })
+        }
+        filters={[new BlurFilter({ strength: blurStrength })]}
+      />
+
+      <pixiSprite texture={texture} layout={{}} />
+    </pixiLayoutContainer>
+  );
+};
+
+const Home = () => {
+  useExtend({ LayoutContainer });
 
   const ref = useRef(undefined);
 
@@ -15,8 +103,7 @@ const LayoutContainer_ = () => {
     const refCurrent = /** @type {pixiJs.Container} */ (ref.current);
 
     refCurrent.getChildAt(0).setMask({
-      mask: refCurrent.getChildAt(1).getChildAt(0),
-      inverse: true
+      mask: refCurrent.getChildAt(1).getChildAt(1)
     });
   }, []);
 
@@ -25,50 +112,34 @@ const LayoutContainer_ = () => {
       ref={ref}
       layout={{
         position: 'relative',
-        borderWidth: 0,
-        borderColor: 0x00ff00
-      }}
-    >
-      <pixiGraphics
-        draw={(graphics) =>
-          graphics.rect(0, 0, dimension, dimension).fill({ color: 0x00ff00 })
-        }
-        layout={{}}
-      />
-
-      <pixiLayoutContainer
-        layout={{
-          position: 'absolute',
-          width: '100%',
-          height: '100%',
-          justifyContent: 'center',
-          alignItems: 'center'
-        }}
-      >
-        <pixiGraphics
-          draw={(graphics) =>
-            graphics.star(0, 0, 5, dimension / 2).fill({ color: 0x0000ff })
-          }
-          layout={{}}
-        />
-      </pixiLayoutContainer>
-    </pixiLayoutContainer>
-  );
-};
-
-const Home = () => {
-  useExtend({ LayoutContainer });
-
-  return (
-    <pixiLayoutContainer
-      layout={{
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
         borderWidth: 0,
         borderColor: 0xff0000
       }}
+      eventMode='static'
+      cursor='pointer'
+      onPointerMove={
+        /** @type {(event: pixiJs.FederatedPointerEvent) => void} */
+        ({ client, currentTarget }) => {
+          const currentTargetLayoutContainer = currentTarget.getChildAt(1);
+
+          Object.assign(
+            currentTargetLayoutContainer,
+            /** @type {pixiJs.ContainerOptions} */ ({
+              position: (() => {
+                const { x, y } = client;
+
+                const { width, height } = currentTargetLayoutContainer;
+
+                return { x: x - width / 2, y: y - height / 2 };
+              })()
+            })
+          );
+        }
+      }
     >
+      <Sprite_ />
+
       <LayoutContainer_ />
     </pixiLayoutContainer>
   );
